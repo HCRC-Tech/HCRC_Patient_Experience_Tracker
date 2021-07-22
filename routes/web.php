602bdeeae81c7b84838d72ec05bbcfab2084cb86
Minhajur Rahman <ridwanminhaj@gmail.com>
2021-07-22 19:21:30 -0300

    Fixes for view patient profile in admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'App\Http\Controllers\DashboardController@index');
+Route::get('/', 'App\Http\Controllers\DashboardController@index')->name('/');
 
 
 
